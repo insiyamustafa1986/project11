3cd1d7d7a93aa136bcd250fc192588966da7ca72
insiyamustafa1986 <88931954+insiyamustafa1986@users.noreply.github.com>
2021-08-18 21:28:19 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+function preload(){
+shipmoving = loadAnimation ("ship-1.png")
+sea=loadImage("sea.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  ship=createsprite(50,160,20,50)
+ship.addImage("moving",shipmoving)
+edge=createEdgeSprite
+ship.scale=0.5
+ship.X=50
+}
+
+
+function draw() {
+  background("blue");
+ if(SafeArray.X )
+sea.X = sea.width/2
+ }
